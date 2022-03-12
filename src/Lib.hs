42aede46972b10f481ebd06db6431c68b664e5db
Antoine Barthelemy <antoine@linux.com>
2022-03-12 16:40:57 +0100

    IO test with usage

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,19 @@
 module Lib
-    ( someFunc
+    ( usage
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import System.Environment (getProgName)
+
+usage :: IO ()
+usage = do
+  name <- getProgName
+  let message = "usage: " ++ name ++ " [-h] jsonfile input\n\
+\\n\
+\positional arguments:\n\
+\  jsonfile\tjson description of the machine\n\
+\\n\
+\  input\t\tinput of the machine\n\
+\\n\
+\optional arguments:\n\
+\  -h, --help show this help message and exit"
+  putStrLn message
