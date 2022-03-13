94aec75e3c6aa385b2b87c0e9b23044e1049cf04
Antoine Barthelemy <antoine@linux.com>
2022-03-13 21:11:54 +0100

    feat(types): parsing on the go

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,8 +2,11 @@ module Lib
     ( usage
     ) where
 
+import Data.Aeson
+
 {- Turing Machine Name -}
-type Name     = String
+type Name = String
+data TapeAction = LEFT | RIGHT | HALT deriving (Enum)
 
 {- Alphabet -}
 type Symbol   = Char
@@ -11,14 +14,23 @@ type Alphabet = [Symbol]
 type Blank    = Symbol {- need to be an element fron the Alphabet type -}
 
 {- States -}
-type State    = String {- will need to be defined as a parsed element -> a record -}
+data State    = {- will need to be defined as a parsed element -> a record -}
+  State { stateName   :: String
+        , transitions :: [TransitionFunc]
+          } deriving (Show, Eq)
+
 type States   = [State]
 type Initial  = State
 type Finals   = [State]
 
 {- Fonctions de transitions -}
 {- Put here -}
-
+data TransitionFunc =
+  TransitionFunc { read    :: Symbol
+                 , toState :: State
+                 , write   :: Symbol
+                 , headTo  :: TapeAction
+                   } deriving (Show, Eq)
 
 {- Deterministic Turing Machine Definition -}
 data Machine =
