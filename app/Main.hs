945b7a88b4252a1658ff6fa8f3132cb144b9ffc1
Antoine Barthelemy <antoine@linux.com>
2022-03-13 12:17:26 +0100

    go for argparse

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,15 @@
 module Main where
 
+import System.Environment
+
 import Lib
 
+{-
+-}
+
 main :: IO ()
-main = usage
+main = do
+  args <- getArgs
+  let phrase = foldr (\x -> (++ " " ++ x)) [] args
+  putStrLn (phrase)
+  usage
